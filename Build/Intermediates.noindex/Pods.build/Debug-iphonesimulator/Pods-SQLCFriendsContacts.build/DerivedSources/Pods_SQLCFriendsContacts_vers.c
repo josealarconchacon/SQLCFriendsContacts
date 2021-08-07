7fefe4d8faf3682fd5ff71a7b7dd2d2a42a94c13
Jose Carlos <josealarconchacon@gmail.com>
2021-08-07 14:47:34 -0400

    first commit

---
+++ Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-SQLCFriendsContacts.build/DerivedSources/Pods_SQLCFriendsContacts_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_SQLCFriendsContactsVersionString[];
+ extern const double Pods_SQLCFriendsContactsVersionNumber;
+
+ const unsigned char Pods_SQLCFriendsContactsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_SQLCFriendsContacts  PROJECT:Pods-1" "\n";
+ const double Pods_SQLCFriendsContactsVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Pods/Target Support Files/Pods-SQLCFriendsContacts/Pods-SQLCFriendsContacts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SQLCFriendsContacts : NSObject
+@end
+@implementation PodsDummy_Pods_SQLCFriendsContacts
+@end
