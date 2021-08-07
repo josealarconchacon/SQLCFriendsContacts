602c34cf2b8c6b0791ed099e1136fcd9411b593c
Jose Carlos <josealarconchacon@gmail.com>
2021-08-07 15:32:07 -0400

    create table function

--- SQLCFriendsContacts/SQLData/SQLiteCommands.swift
+++ SQLCFriendsContacts/SQLData/SQLiteCommands.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SQLite
+
+class SQLiteCommand {
+    static var set_table = Table("contact")
+    
+    // expression
+    static let id = Expression<Int>("id")
+    static let first_name = Expression<String>("first_name")
+    static let last_name = Expression<String>("last_name")
+    static let phone_number = Expression<Int>("phone_number")
+    static let user_image = Expression<Data>("user_image")
+    static let user_email = Expression<String>("user_email")
+    
+    // create table
+    static func create_table() {
+        guard let database = SQLDB.share.data_base else {
+            print("Data connection error: ")
+            return
+        }
+        do {
+            // create table if Not Exists
+            try database.run(set_table.create(ifNotExists: true) { table in
+                table.column(id, primaryKey: true)
+                table.column(first_name)
+                table.column(last_name)
+                table.column(phone_number, unique: true)
+                table.column(user_image)
+                table.column(user_email)
+            })
+        } catch {
+            print("Table already exists: \(error.localizedDescription)")
+        }
+    }
+    
+    // iNSERTING ROW
+    static func insert_row(_ contact: UserContact) -> Bool? {
+        guard let db = SQLDB.share.data_base  else {
+            print("Data store connection error")
+            return nil
+        }
+    }
+}
